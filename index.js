70e05bca580049a96bb5136cfebad7cde77aefe7
FahadJanahi10 <fahad.a.janahi@gmail.com>
2022-10-16 10:14:02 +0300

    animals page done

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require("express")
 const app = express()
-const PORT = 8000
+const PORT = 3000
+const ejsLayouts = require("express-ejs-layouts")
 
+// middleware
 app.set("view engine", "ejs")
+app.use(ejsLayouts)
 
+// route
+app.get("/", (req, res)=>{
+    res.render("home.ejs")
+})
+
+app.get("/animals", (req, res)=>{
+    res.render("animals.ejs", {animals: ["dogs", "cats", "monkeys", "alligators", "dinasors"]})
+})
+
+app.listen(PORT, ()=>{
+    console.log("hi")
+})
