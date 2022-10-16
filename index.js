418ffdd191830d2522434686bcd3e2e9a4ad2b3b
FahadJanahi10 <fahad.a.janahi@gmail.com>
2022-10-16 10:55:46 +0300

    nav and stuff

--- index.js
+++ index.js
@@ -7,13 +7,29 @@ const ejsLayouts = require("express-ejs-layouts")
 app.set("view engine", "ejs")
 app.use(ejsLayouts)
 
-// route
+// ROUTES
+// home
 app.get("/", (req, res)=>{
     res.render("home.ejs")
 })
-
+// animals
 app.get("/animals", (req, res)=>{
     res.render("animals.ejs", {animals: ["dogs", "cats", "monkeys", "alligators", "dinasors"]})
+})
+
+// foods
+app.get("/foods", (req, res)=>{
+    res.render("foods.ejs", {foods: ["moms food", "healthy calory"]})
+})
+
+// movies
+app.get("/movies", (req, res)=>{
+    res.render("movies.ejs", {movies: ["south park"]})
+})
+
+// products
+app.get("/products", (req, res)=>{
+    res.render("products.ejs", {products: ["shampoo", "coffee"]})
 })
 
 app.listen(PORT, ()=>{
